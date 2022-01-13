0c201c8977aca70b9815ff89bfae65c172c774bb
Phil Ashton <pmash2@users.noreply.github.com>
2022-01-13 09:04:26 -0500

    Code cleanup

--- src/Types/time.ts
+++ src/Types/time.ts
@@ -1,34 +1,34 @@
 export default class Time {
-    public Hours: number
-    public Minutes: number
-    public Seconds: number
-    public Milliseconds: number
+	public Hours: number
+	public Minutes: number
+	public Seconds: number
+	public Milliseconds: number
 
-    constructor(h: number, m: number, s: number, ms: number){
-        this.Hours = h
-        this.Minutes = m
-        this.Seconds = s
-        this.Milliseconds = ms
-    }
+	constructor(h: number, m: number, s: number, ms: number) {
+		this.Hours = h
+		this.Minutes = m
+		this.Seconds = s
+		this.Milliseconds = ms
+	}
 
-    ToString = (showMS: boolean = true): string => {
-        let timeString = ""
+	ToString = (showMS: boolean = true): string => {
+		let timeString = ""
 
-        if (this.Hours) {
-            timeString = this.zeroPad(this.Hours, 2) + ":"
-        }
+		if (this.Hours) {
+			timeString = this.zeroPad(this.Hours, 2) + ":"
+		}
 
-        timeString += this.zeroPad(this.Minutes, 2) + ":"
-        timeString += this.zeroPad(this.Seconds, 2)
+		timeString += this.zeroPad(this.Minutes, 2) + ":"
+		timeString += this.zeroPad(this.Seconds, 2)
 
-        if (showMS) {
-            timeString += "." + this.zeroPad(this.Milliseconds, 3)
-        }
+		if (showMS) {
+			timeString += "." + this.zeroPad(this.Milliseconds, 3)
+		}
 
-        return timeString
-    }
+		return timeString
+	}
 
-    private zeroPad = (numberToPad: number, totalCharLength: number): string => {
-        return (`0${numberToPad}`).slice(-totalCharLength)
-    }
+	private zeroPad = (numberToPad: number, totalCharLength: number): string => {
+		return `0${numberToPad}`.slice(-totalCharLength)
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,11 @@ import TimeUtilities from './time-formatter'
 import PomodoroState from "./Types/pomodoro-state"
 
 export { Time, Timer, Pomodoro, EmitString, TimeUtilities, PomodoroState }
-export const getTimer = (h: number, m: number, s: number, ms: number): Timer => 
-    new Timer(new Time(h, m, s, ms))
 
-export const getRemainingTime = (timer: Timer): string => {
-    return timer.Remaining.ToString()
-}
+export const getTimer = (h: number, m: number, s: number, ms: number): Timer =>
+	new Timer(new Time(h, m, s, ms))
+
+export const getRemainingTime = (timer: Timer): string => timer.Remaining.ToString()
 
 export const getPomodoro = (wrk: Timer, brk: Timer): Pomodoro =>
     new Pomodoro(wrk, brk)
